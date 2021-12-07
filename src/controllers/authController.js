3e4745f0bdf0f7d0aba96d8cb12359b826225ddc
Aryan <aryanagrawal20023@gmail.com>
2021-12-07 18:01:19 +0530

    bcrypt and jwt setup completed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,39 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+
+module.exports.login_get = (req, res)=>{
+    console.log("Login Page Up");
+}
+
+module.exports.login_post = async (req, res) =>{
+    const email = req.body.email;
+    const textPassword = req.body.password;
+
+    const user = await User.findOne({ email }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(textPassword, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				email: user.email
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+}
 
 module.exports.signup_get = (req, res)=>{
     console.log("SignUp Page Up");
@@ -6,14 +41,34 @@ module.exports.signup_get = (req, res)=>{
 
 module.exports.signup_post = async (req, res) => {
     const email = req.body.email;
-    const password = req.body.password;
+    const textPassword = req.body.password;
+
+    if (!email || typeof email !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!textPassword || typeof textPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (textPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+    const password = await bcrypt.hash(textPassword, 10);
     try {
       const user = await User.create({ email, password });
-      res.status(201).json(user);
+      console.log('User created successfully: ', user)
     }
     catch(err) {
-      console.log(err);
-      res.status(400).send('error, user not created');
+        if (err.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw err
     }
    
   }
--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -3,6 +3,8 @@ const authController = require('../controllers/authController');
 
 const router = Router();
 
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
 router.get('/signup', authController.signup_get);
 router.post('/signup', authController.signup_post);
 
