da0b29eb54bd93dabc58348af1a8a60b8d206e42
Vedant <daigavane70@gmail.com>
2021-12-18 16:07:38 +0530

    Contest: Get All done

--- src/controllers/contest.js
+++ src/controllers/contest.js
@@ -7,17 +7,28 @@ async function NewContest(req, res) {
 
   console.log("Creating");
 
-  try{
+  try {
     if (title && startsOn && endsOn) {
-        const newContest = await Contest.create({ title, startsOn, endsOn });
-        res.send(newContest);
-      } else {
-        res.status(400).send("Unable to create the contest");
-      }
-  }
-  catch(err){
-      res.status(400).send("An error occured")
+      const newContest = await Contest.create({ title, startsOn, endsOn });
+      res.send(newContest);
+    } else {
+      res
+        .status(400)
+        .send("Invalid data received, please send title, startsOn, endsOn");
+    }
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 }
 
-module.exports = { NewContest };
+//get all contest
+async function GetAllContest(req, res) {
+  try {
+    const allContests = await Contest.find({});
+    res.send(allContests);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+}
+
+module.exports = { NewContest, GetAllContest };
--- src/routers/contest.router.js
+++ src/routers/contest.router.js
@@ -1,6 +1,7 @@
-const { NewContest } = require("../controllers/contest");
+const { NewContest, GetAllContest } = require("../controllers/contest");
 const ContestRouter = require("express").Router();
 
 ContestRouter.post("/", NewContest);
+ContestRouter.get("/", GetAllContest);
 
 module.exports = ContestRouter;
