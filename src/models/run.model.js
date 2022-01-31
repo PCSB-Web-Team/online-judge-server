e6b2f184a4102627d7e5086e20255d6a6eeb27c6
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-01-31 13:05:24 +0530

    Run: Model added

---
+++ src/models/run.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const runSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+    contestId: {
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+    questionId: {
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+    token: {
+        type: String,
+    },
+    stdout: {
+      type: String,
+      default: null,
+    },
+    time: {
+      type: mongoose.Types.Decimal128,
+      default: 0,
+    },
+    memory: {
+      type: Number,
+      default: 0,
+    },
+    stderr: {
+      type: String,
+      default: null,
+    },
+    compile_output: {
+      type: String,
+      default: null,
+    },
+    message: {
+      type: String,
+      default: null,
+    },
+    status: {
+      id: { type: String, default: 0 , required: true},
+      description: { type: String, default: "Processing", required: true},
+    },
+  },
+  { _id: true, strict: false }
+);
+
+const RunModel = mongoose.model("run", runSchema);
+
+module.exports = SubmissionModel;
