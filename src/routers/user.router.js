46d403df792d96720f8fc1d8969dcfbaf8f9ce02
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-02-01 13:21:25 +0530

    Users: Removed user route and controller

--- src/routers/user.router.js
+++
@@ -1,6 +0,0 @@
-const { registerContest } = require("../controllers/user.controller");
-const UserRouter = require("express").Router();
-
-UserRouter.put("/registerContest", registerContest);
-
-module.exports = UserRouter;
