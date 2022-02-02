bd8ca8f5f3949a7eaa4ea3ae60573c8ce2ca0c79
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-02-02 15:06:36 +0530

    Run: Timeout added

--- src/utility/run.queue.js
+++ src/utility/run.queue.js
@@ -3,24 +3,35 @@ const { runBatch } = require("./judge0");
 
 const runQueue = new Bull("run", {
   redis: "redis:6379",
-  limiter: {
-    max: 5,
-    duration: 5000,
-  },
 });
 
 var list = [];
 
+var timeOut;
+
+// this function will create a timeout promise and store the value in timeOut variable
+function startTimeOut() {
+  timeOut = setTimeout(() => {
+    if (list.length) runBatch(list);
+  }, 10000);
+}
+
 // Redis Consumer : Executing after Producer adds data to queue
 const runProcess = async (job) => {
+
+  // first clear the timeout that is running
+  clearTimeout(timeOut);
 
   // Push data to array list
   list.push(job.data);
 
-  // If list length is n then send for Batch Submission (Judge0)
+  // If list length is n then send for Batch Run (Judge0)
   if (list.length == 5) {
     runBatch(list);
     list = [];
+  } else {
+    // start the timeout again for next 10 seconds
+    startTimeOut();
   }
 };
 
