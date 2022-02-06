87b8dcd9f4d9dbb81458a034d767d77340ab4c06
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-02-06 12:52:17 +0530

    Run: Callback queue implemented

--- src/controllers/callback.controller.js
+++ src/controllers/callback.controller.js
@@ -3,6 +3,7 @@ const Execution = require("../models/execution.model");
 const Run = require("../models/run.model");
 const { UpdateScore } = require("../controllers/participant.controller");
 const { subCallBackQueue } = require("../utility/subCallBack.queue");
+const { runCallBackQueue } = require("../utility/runCallBack.queue");
 
 // Receive data from Judge0 and update it in Execution
 // Then count passed testCases submissions and update score in Submission Model
@@ -16,38 +17,7 @@ async function subCallBackHandler(req, res) {
 // Note: Data received here is through PUT request on ./callback/run by Judge0
 
 async function runCallBackHandler(req, res) {
-  try {
-    const callbackBody = req.body;
-
-    // Decoding all the Base64 encoded fields
-    callbackBody.stdout = Buffer.from(
-      callbackBody.stdout || "",
-      "base64"
-    ).toString("ascii");
-    callbackBody.message = Buffer.from(
-      callbackBody.message || "",
-      "base64"
-    ).toString("ascii");
-    callbackBody.stderr = Buffer.from(
-      callbackBody.stderr || "",
-      "base64"
-    ).toString("ascii");
-    callbackBody.compile_output = Buffer.from(
-      callbackBody.compile_output || "",
-      "base64"
-    ).toString("ascii");
-
-    // Update the Run Model with body
-    const runBody = await Run.findOneAndUpdate(
-      { token: callbackBody.token },
-      callbackBody,
-      { upsert: true }
-    );
-
-    res.send("Done");
-  } catch (err) {
-    res.status(400).send("Error: " + err.message);
-  }
+  runCallBackQueue.add(req.body);
 }
 
 module.exports = { subCallBackHandler, runCallBackHandler };
