9364eed7f036e5d746b2df5fa85bb8cd7f215039
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-02-12 22:26:43 +0530

    Submission: testing

--- src/models/submission.model.js
+++ src/models/submission.model.js
@@ -29,10 +29,6 @@ const submissionSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    checkedCases: {
-      type: Number,
-      default: 0,
-    },
     timestamp: { type: Date, default: Date.now},
   },
   { _id: true, strict: false }
