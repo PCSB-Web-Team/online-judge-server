82f10ff3350ae8aacfb0a363bfb760b80016d6e3
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-06-06 22:38:19 +0530

    Shell Sripts updated

--- stop.sh
+++ stop.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
 # Stop Judge0
-sudo docker-compose -f docker-compose.judge0.yml down
-
-#Stop Django
+cd ../judge0-v1.13.0/
 sudo docker-compose down
+
+#Stop Node
+cd ../online-judge-server/
+sudo docker-compose down
+echo 'Judge0 & Server Down'
