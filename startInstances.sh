d3521a1427a4b622025789ae2968de3fb9b86e60
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-08-07 13:40:02 +0530

    multiple instance added

---
+++ startInstances.sh
@@ -0,0 +1,79 @@
+cd instance2359
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2360
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2361
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2362
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2363
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2364
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2365
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2366
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2367
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
+
+cd instance2368
+sudo docker-compose down
+sudo docker-compose up -d db redis
+sleep 10s
+sudo docker-compose up -d
+sleep 5s
+cd ..
